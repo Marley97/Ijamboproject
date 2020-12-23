96c94a65ec6a37ec7244a2f0ef88d2e7534bcda9
Marley97 <hermesmarleyngenda@gmail.com>
2020-12-23 21:58:54 +0100

    Etablissement de deux models Music et Album avec ses composants

--- ijamboAdmin/urls.py
+++ ijamboAdmin/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from . import views
+from .import views
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.dashboard, name='dashboard'),
+    path('umuziki/', views.music, name='imiziki'),
+    path('urukurikirane/', views.musicalbum, name='albumu'),
+    path('iyukwezi/', views.songmonth, name='yukwezi'),
+    path('listMusic/', views.ListMusic, name='listeM'),
+    path('show/', views.EventRegester, name='ikirori'),
+    
     
 ]
--- ijamboAdmin/views.py
+++ ijamboAdmin/views.py
@@ -1,13 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
-from ijambo.models import *
-# Create your views here.
+from .models import *
+from .forms import *
+from django.contrib.auth.decorators import login_required
 
-def home(request):
-	try:
-		profile = Profil.objects.get(user=request.user)
-		return render(request, 'simple_user.html', locals())
-	except Exception as e:
-		err_msg = "You must login first to access this page !"
-		return render(request, '404.html', locals())
-	
+@login_required(login_url='login')
+def dashboard(request):
+	pro=Profil.objects.get(user=request.user)
+	return render(request, 'simple_user.html', locals())
+
+@login_required(login_url='login')	
+def music(request):
+	usr=request.user
+	musique_form = MusicForm(request.POST or None,request.FILES)
+	if(request.method=='POST'):
+		if(musique_form.is_valid()):
+			hh = musique_form.save(commit=False)
+			hh.author = usr
+			hh.save()
+			return redirect('listeM')
+	musique_form = MusicForm()
+	return render(request,"forms.html",locals())
+
+@login_required(login_url='login')
+def musicalbum(request):
+	usr=request.user
+	musiquealbum_form = AlbumForm(request.user,request.POST or None,request.FILES)
+	if(request.method=='POST'):
+		if(musiquealbum_form.is_valid()):
+			bb = musiquealbum_form.save(commit=False)
+			bb.author=usr
+			bb.save()
+	musiquealbum_form = AlbumForm(request.user)
+	return render(request,"forms.html",locals())
+
+def songmonth(request):
+	musiquemois_form = MonthSongForms(request.POST or None,request.FILES)
+	if(request.method=='POST'):
+		if(musiquemois_form.is_valid()):
+			musiquemois_form.save()
+	musiquemois_form = MonthSongForms()
+	return render(request,"forms.html",locals())
+
+def ListMusic(request):
+	musics = Music.objects.filter(author=request.user)
+	return render(request, "listes.html", locals())
+
+@login_required(login_url='login')
+def EventRegester(request):
+	InputEvent = EventForm(request.POST or None, request.FILES)
+	if(request.method == 'POST'):
+		if(InputEvent.is_valid()):
+			cc = InputEvent.save(commit = False)
+			cc.author = request.user
+			cc.save()
+	InputEvent = EventForm()
+	return render(request, "forms.html", locals())
+
+
+
+
